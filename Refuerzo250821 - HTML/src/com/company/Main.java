034c509f4e7579376793e8270b0dda906d65ab93
Yania Eszter Dávid <3855921830108@ingenieria.usac.edu.gt>
2021-08-25 20:19:51 -0600

    25/08/2021 - Creación de Archivos HTML

--- Refuerzo250821 - HTML/src/com/company/Main.java
+++ Refuerzo250821 - HTML/src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -46,10 +43,14 @@ public class Main {
         return "";
     }
 
-    public static void main(String[] args) {
+        public static String[] nombres;
+        public static int[] notas;
+        public static int cr, ca;
 
-        String[]nombres = new String[10];
-        int[]notas = new int[10];
+
+    public static void main(String[] args) {
+        nombres = new String[10];
+        notas = new int[10];
 
         System.out.println("ingrese una ruta");
         String ruta = new Scanner(System.in).nextLine();
@@ -69,13 +70,13 @@ public class Main {
             System.out.println("nombre: "+nombres[i]+" nota: "+notas[i]);
 
         }
-        int cr=0;
+        cr=0;
         for (int i = 0; i < notas.length; i++) {
             if (notas[i]<61){
                 cr++;
             }
         }
-        int ca=0;
+        ca=0;
         for (int i = 0; i <notas.length ; i++) {
             if (notas[i]>60){
                 ca++;
@@ -87,5 +88,110 @@ public class Main {
         System.out.println("cantidad de alumnos reprobados: "+cr);
         System.out.println("cantidad de alumnos aprobados: "+ca);
 
+        GenerarReporte();
+
+    }
+
+    public static int[] vector1 = {4,5,2,7,8};
+    public static int[] vector2 = {7,9,1,0,9};
+
+    //Método para crear archivos
+    public static void GenerarReporte(){
+        System.out.println("Ingrese la ruta donde desea guardar su reporte: ");
+        String rutaReporte = new Scanner(System.in).nextLine();
+
+        //Generación del reporte en java:
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+
+        String texto = "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "\t<meta charset=\"utf-8\">\n" +
+                "\t<title>Ejemplo reporte</title>\n" +
+                "\n" +
+                "<style type=\"text/css\">\n" +
+                "body{\n" +
+                "\tmargin: 0;\n" +
+                "\tfont-family: Trebuchet MS, sans-serif;\n" +
+                "\tbackground-color: #fefbe9;\n" +
+                "}\n" +
+                ".topnav {\n" +
+                "\toverflow: hidden;\n" +
+                "\tbackground-color: #DC143C;\n" +
+                "\ttext-align: center;\n" +
+                "}\n" +
+                "table {\n" +
+                "\tborder-collapse: collapse;\n" +
+                "\twidth: 50%;\n" +
+                "}\n" +
+                "td, th {\n" +
+                "\tfont-family: bahnschrift;\n" +
+                "\tborder: 1px solid #000;\n" +
+                "\ttext-align: center;\n" +
+                "\tpadding: 8px;\n" +
+                "}\n" +
+                "\n" +
+                "</style>\n" +
+                "\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "\t<div align=\"center\" class=\"topnav\"> \n" +
+                "\t<h1>REPORTE</h1><p>REFUERZO IPC 1</p>\n" +
+                "\t</div><br><br>\n" +
+                "\n" +
+                "<div class=\"tabla_Clase\" align=\"center\" style=\"text-align: center;\">\t\n" +
+                "\t<table align=\"center\">\n" +
+                "\t\t<tr>\n" +
+                "\t\t\t<th colspan=\"2\" style=\"background-color: #000000; color: white;\">Listado de alumnos</th>\n" +
+                "\t\t</tr>\n" +
+                "\t\t<tr style=\"font-weight: bold; background-color: #57D4CB;\">\n" +
+                "\t\t\t<th>Nombre</th><th>Nota</th>\n"+
+                "\t\t</tr>\n";
+
+            for (int i = 0; i < nombres.length; i++) {
+                texto += "\t\t<tr><td style=\"background-color: white\">"+ nombres[i] +"</td>\n" +
+                        "\t\t<td style=\"background-color: white\">"+ notas[i] +"</td></tr>\t\n" ;
+            }
+
+                texto += "\t</table>\n" +
+                "</div>\n" +
+                "<div class=\"texto\" align=\"center\" style=\"text-align: center;\">\n" +
+                "\t<p>La cantidad de alumnos reprobados para este curso son:</p>\n" +
+                "\t<p style=\"color: green; font-weight: bold\">"+ cr +"</p>\n" +
+                "\t<p>La cantidad de alumnos aprobados para este curso son:</p>\n" +
+                "\t<p style=\"color: red; font-weight: bold\">"+ ca +"</p>\n"+
+                "<br><h2>VER VECTORES</h2>";
+
+
+                for (int i = 0; i < vector2.length; i++) {
+                    texto += "<p>["+vector1[i]+"] &nbsp&nbsp&nbsp&nbsp ["+vector2[i]+"]</p>";
+                }
+
+                texto += "\t</div>\n" +
+                "</body>\n" +
+                "</html>";
+
+
+        try
+        {
+            fichero = new FileWriter(rutaReporte+"\\reporte.html");
+            pw = new PrintWriter(fichero);
+
+            for (int i = 0; i <= 0; i++)
+                pw.println(texto);
+
+
+            System.out.println("Reporte generado.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fichero)
+                    fichero.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
     }
 }
