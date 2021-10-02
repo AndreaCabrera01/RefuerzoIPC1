bf4e2bab447435f393fe890f63efa2590593bb85
Yania Eszter Dávid <3855921830108@ingenieria.usac.edu.gt>
2021-10-01 19:10:43 -0600

    Ejemplo Perrito Pt2

---
+++ EjemploPerrito/src/com/company/Hueso.java
@@ -0,0 +1,10 @@
+package com.company;
+import javax.swing.*;
+
+public class Hueso extends JLabel{
+    public Hueso(){
+        this.setIcon(new ImageIcon("hueso.png"));
+        this.setSize(128,128);
+    }
+
+}
--- EjemploPerrito/src/com/company/Interfaz.java
+++ EjemploPerrito/src/com/company/Interfaz.java
@@ -2,22 +2,34 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class Interfaz extends JFrame {
+
+
+public class Interfaz extends JFrame implements ActionListener{
     JButton tirar;
     JLabel perrito;
+
+    public static Hueso hueso;
+    public static int x, y;
+
     public Interfaz(){
+        hueso = new Hueso();
+
         this.setTitle("Juego del Perrito");
-        this.setBounds(400, 250, 1200, 900);
+        this.setBounds(400, 250, 1200, 600);
         this.getContentPane().setBackground(new Color(82, 173, 200));
         this.setLayout(null);
         this.setLocationRelativeTo(null);
 
         tirar = new JButton("Tirar Hueso");
-        tirar.setBounds(520, 725, 200, 50);
+        tirar.setBounds(520, 505, 200, 50);
         tirar.setFont(new Font("Century Gothic", Font.BOLD, 20));
         tirar.setForeground(Color.white);
         tirar.setBackground(new Color(85, 195, 158));
+        tirar.addActionListener(this);
 
         //------------LABELS DEL PERRITO---------------
         perrito = new JLabel(new ImageIcon("perro.png"));
@@ -28,5 +40,16 @@ public class Interfaz extends JFrame {
         this.add(perrito);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == tirar){
+            Random random = new Random();
+            x = (int) (random.nextDouble() * 1072);
+            y = (int) (random.nextDouble() * 472);
+            hueso.setLocation(x,y);
+            this.add(hueso);
+        }
     }
 }
